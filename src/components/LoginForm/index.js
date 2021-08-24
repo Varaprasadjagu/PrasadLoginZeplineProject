2a64e92d9d11bdaf412e3e21eb440000276c205c
prasad <varaprasadjagu@gmail.com>
2021-08-24 06:37:50 +0000

    prasadLoginZeplin

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,39 @@
+import {Component} from 'react'
+
+import Home from '../Home'
+
+import {Component} from 'react'
+import HomePage from '../HomePage'
+import Signin from '../Signin'
+
+class LoginForm extends Component {
+  state = {
+    isLogin: false,
+  }
+
+  onLoginSuccess = () => {
+    this.setState({isLogin: true})
+  }
+  onClickLogout = () => {
+    this.setState({isLogin: false})
+  }
+  render() {
+    const {isLogin} = this.state
+    const {SignupFunction, maintainData} = this.props
+    return (
+      <div>
+        {isLogin ? (
+          <HomePage onClickLogout={this.onClickLogout} />
+        ) : (
+          <Signin
+            SignupFunction={SignupFunction}
+            onLoginSuccess={this.onLoginSuccess}
+            maintainData={maintainData}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default LoginForm
